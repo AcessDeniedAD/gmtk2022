f0966fa7b69367cdcd3e88fe9e486dd1d2132290
jipey25 <chevalier.lancioni.jp@gmail.com>
2022-07-16 03:19:26 +0200

    [Feat] Add Jump Refactored some of the code, could be more refactored

--- Assets/Andros/Scripts/MonoBehavior/Player/PlayerController.cs
+++ Assets/Andros/Scripts/MonoBehavior/Player/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : BaseLivingObject
 
     private MainPlayerInput _mainPlayerInput;
 
+
     private void Awake()
     {
         _mainPlayerInput = new MainPlayerInput();
@@ -38,36 +39,26 @@ public class PlayerController : BaseLivingObject
         _mainPlayerInput.Disable(); 
     }
 
-    private void FixedUpdate()
-    {
-        _playerMovement.MoveAlongDirection(PlayerDirection);
-        Vector3 movement = Vector3.right * PlayerDirection.x + Vector3.forward * PlayerDirection.y;
-        if(movement != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(movement, transform.up);
-        }
-    }
-
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            PlayerDirection = context.ReadValue<Vector2>();
+            _playerMovement.PlayerDirection = context.ReadValue<Vector2>();
         }
         else if (context.canceled)
         {
-            PlayerDirection = Vector2.zero;
+            _playerMovement.PlayerDirection = Vector2.zero;
         }
     }
 
     public void OnDash(InputAction.CallbackContext context)
     {
-        _playerMovement.Dash(PlayerDirection);
+        _playerMovement.Dash();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        Debug.Log("Should jump");
+        _playerMovement.Jump();
     }
 
 }
--- Assets/Andros/Scripts/MonoBehavior/Player/PlayerMovement.cs
+++ Assets/Andros/Scripts/MonoBehavior/Player/PlayerMovement.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Vector2 PlayerDirection;
+
     [SerializeField]
     private float _playerSpeed = 0.5f;
 
@@ -16,30 +19,80 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _dashCoolDown = 2f;
 
+    [SerializeField]
+    private float _gravity = -9.81f;
+
+    [SerializeField]
+    private float _jumpForce = 10f;
+
+    [SerializeField]
+    private float verticalVelocity;
+
+    private float groundDistanceCheck = 0.5f;
+
     private bool _isDashing = false;
 
     private bool _canDash = true;
 
-    public void MoveAlongDirection(Vector2 playerDirection)
+    public bool _isGrounded = false;
+
+    public bool _hasJumped = false;
+
+    private void FixedUpdate()
     {
-        transform.position += new Vector3 (playerDirection.x, 0, playerDirection.y) * _playerSpeed * Time.deltaTime * Time.timeScale;
+        verticalVelocity += _gravity * Time.deltaTime;
+        Vector3 movement = Vector3.right * PlayerDirection.x + Vector3.forward * PlayerDirection.y;
+        MoveAlongDirection(movement);
+        AdjustOrientation(movement);
+        ChackIfGrounded();
+        ApplyGravity(verticalVelocity);
     }
 
-    public void Dash(Vector2 playerDirection)
+    private void ChackIfGrounded()
     {
-        if(_canDash)
+        if (CheckCollision(Vector3.down) && verticalVelocity < 0)
         {
-            StartCoroutine(DashCoroutine(playerDirection));
+            verticalVelocity = 0;
+            _isGrounded = true;
+        }
+        else
+        {
+            _isGrounded = false;
         }
     }
 
-    private IEnumerator DashCoroutine(Vector2 playerDirection)
+    private bool CheckCollision(Vector3 direction)
+    {
+        RaycastHit Hit;
+        return Physics.Raycast(transform.position, direction, out Hit, groundDistanceCheck);
+    }
+
+    public void ApplyGravity(float currentVelocity)
+    {
+        Debug.Log(currentVelocity);
+        transform.Translate(new Vector3(0, currentVelocity, 0) * Time.fixedDeltaTime);
+    }
+
+    public void MoveAlongDirection(Vector3 playerDirection)
+    {
+        transform.position += playerDirection * _playerSpeed * Time.fixedDeltaTime * Time.timeScale;
+    }
+
+    public void Dash()
+    {
+        if(_canDash)
+        {
+            StartCoroutine(DashCoroutine());
+        }
+    }
+
+    private IEnumerator DashCoroutine()
     {
         _canDash = false;
         var _remainingDashTime = _dashTime;
         while(_remainingDashTime > 0)
         {
-            transform.position += new Vector3(playerDirection.x, 0, playerDirection.y) * _dashSpeed * Time.fixedDeltaTime * Time.timeScale;
+            transform.position += new Vector3(PlayerDirection.x, 0, PlayerDirection.y) * _dashSpeed * Time.fixedDeltaTime * Time.timeScale;
             _remainingDashTime -= Time.fixedDeltaTime;
             yield return new WaitForFixedUpdate();
         }
@@ -52,7 +105,19 @@ public class PlayerMovement : MonoBehaviour
         _canDash = true;
     }
 
+    private void AdjustOrientation(Vector3 playerDirection)
+    {
+        if (playerDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(playerDirection, transform.up);
+        }
+    }
+
     public void Jump()
     {
+        if( _isGrounded)
+        {
+            verticalVelocity = _jumpForce;
+        }
     }
 }
